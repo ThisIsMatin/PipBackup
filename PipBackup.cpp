d9404b4a1cc1d8193d40351e89a00558fe8feea9
Matin Najafi <matinnajafi.dev@gmail.com>
2021-02-06 11:19:20 +0330

    Initial commit

---
+++ PipBackup.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <sstream>
+#include <fstream>
+#include <conio.h> 
+#include <ctime>
+
+using namespace std;
+
+string ssystem(const char *command) {
+    char tmpname [L_tmpnam];
+    tmpnam ( tmpname );
+    string scommand = command;
+    string cmd = scommand + " >> " + tmpname;
+    system(cmd.c_str());
+    ifstream file(tmpname, ios::in | ios::binary );
+    string result;
+    if (file) {
+        while (!file.eof()) result.push_back(file.get())
+            ;
+        file.close();
+    }
+    remove(tmpname);
+    return result;
+}
+
+string FormatLine(string text)
+{
+int length = text.size();
+int x = 0;
+string fixed;
+
+while (x != length) {
+        string letter = text.substr(x, 1);
+        if (letter == " ") {
+           letter = "==";}
+        fixed = fixed + letter;
+        x = x + 1;
+                    }
+
+return fixed;
+}
+
+int main(int argc, char *argv[])
+{
+    cout <<"Backing up your pip modules..."<<endl;
+    string bash = "pip list";
+    string s = ssystem(bash.c_str());
+    string in;
+    istringstream iss(s);
+    string line;
+    string output;
+    int checker = 0;
+    srand(time(0));
+    int FileID = (rand() % (99999 - 11111)) + 11111;
+    string filename = "backup_"+to_string(FileID)+".txt";
+    ofstream BackupFile(filename);
+    while (getline(iss, line))
+    {
+        checker++;
+        if(checker > 2 && line.length() > 3) {
+            int pos = -1;
+            while ((pos = line.find("  ")) != -1)
+                line.replace(pos, sizeof("  ") - 1, " ");
+            BackupFile << FormatLine(line);
+        }
+    }
+    for (size_t i = 0; i < 30; i++)
+        cout<<'.';
+    cout<<endl;
+    cout<<"Your pip list was successfully backed up in (backup_"+filename+".txt)!"<<endl;
+    cout<<"To install the backup, just use this command : (python -m pip install -r backup_"+filename+".txt)"<<endl;
+    BackupFile.close();
+    getch();
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+<div align='center'>
+  <br />
+  <p>
+    <a href='https://github.com/ThisIsMatin/PipBackup'><img src='https://raw.githubusercontent.com/ThisIsMatin/PipBackup/master/images/logo.png' width='320' alt='Logo' /></a>
+  </p>
+    <br/>
+  <p>
+    <img src='https://img.shields.io/badge/Latest-Version-4f91ff' alt='' />  <img src='https://img.shields.io/badge/Testing-passing-green?logo=github' alt='' /> <img src='https://img.shields.io/badge/++-Build-731601?logo=c' alt='' /> 
+
+  </p>
+</div>
+
+## About
+Pip Backup is a tool for creating a requirements.txt file written in C ++. Just run this tool to create a txt backup file from your list of installed modules for Python.
+
+## How To Use
+Just run the tool to start the backup file.
